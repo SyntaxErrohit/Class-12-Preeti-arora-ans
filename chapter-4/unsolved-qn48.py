cab7776eda043b1da34cca863398730f10872cad
rohit elamurugan <rohitelamurugan1607@gmail.com>
2023-03-06 23:51:35 +0530

    edited to a working code of 48

--- chapter-4/unsolved-qn48.py
+++ chapter-4/unsolved-qn48.py
@@ -1,6 +1,6 @@
 import csv
 
 with open("contacts.csv", "r") as f:
-    objcsv = csv.reader(f, delimiter="")
+    objcsv = csv.reader(f, delimiter=",")
     for i in objcsv:
         print("Name:", i[0], "Phone:", i[1])
