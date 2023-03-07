3d7b6c55d2ede80be330339eda4e09d55ede6f3d
rohit elamurugan <rohitelamurugan1607@gmail.com>
2023-03-07 23:46:30 +0530

    added input

--- chapter-2/unsolved-qn2.py
+++ chapter-2/unsolved-qn2.py
@@ -1,3 +1,7 @@
 def calculate_area(base, height):
     return 0.5 * base * height
 
+b = int(input("Enter base: "))
+h = int(input("Enter height: "))
+a = calculate_area(b, h)
+print("Area of triangle:", a)
