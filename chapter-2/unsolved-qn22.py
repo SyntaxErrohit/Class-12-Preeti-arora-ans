fca7613e013671ace5be01362275e15c514141d5
rohit elamurugan <rohitelamurugan1607@gmail.com>
2023-03-08 23:25:15 +0530

    added 2 cases for qn 22

--- chapter-2/unsolved-qn22.py
+++ chapter-2/unsolved-qn22.py
@@ -1,8 +1,23 @@
+# Recursion
 def add(n):
     if n==1:return 1
     if n%2 == 0:
         return 1/n + add(n-1)
     else:
         return -1/n + add(n-1)
+
+print(add(5))
+
+# Non-recursion
+def add(n):
+    s = 1
+    i = 2
+    while i <= n:
+        if i%2==0:
+            s += 1/i
+        else:
+            s -= 1/i
+        i += 1
+    return s
 
 print(add(5))
