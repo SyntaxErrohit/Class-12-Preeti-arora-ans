79b49959adcb06e3d648fa45fbc5c9b0d3512e20
rohit elamurugan <rohitelamurugan1607@gmail.com>
2023-03-08 23:26:13 +0530

    made while to for loop in qn 22

--- chapter-2/unsolved-qn22.py
+++ chapter-2/unsolved-qn22.py
@@ -11,13 +11,11 @@ print(add(5))
 # Non-recursion
 def add(n):
     s = 1
-    i = 2
-    while i <= n:
+    for i in range(2, n+1):
         if i%2==0:
             s += 1/i
         else:
             s -= 1/i
-        i += 1
     return s
 
 print(add(5))
