bf5dbb65bc232851806aed206102d38441c70624
rohit elamurugan <rohitelamurugan1607@gmail.com>
2023-03-18 22:49:51 +0530

    made

--- chapter-6/stack-operations.py
+++ chapter-6/stack-operations.py
@@ -1,7 +1,8 @@
 stack = list()
 size = 0
 
-def push(item):
+def push():
+    item = input("Enter item: ")
     stack.append(item)
     size += 1
     return size
@@ -15,10 +16,45 @@ def peek():
 
 def isEmpty():
     if size == 0:
-        print("tack is empty")
+        print("Stack is empty")
     else:
         print("Stack is not empty")
 
+def pop():
+    if size == 0:
+        print("Underflow, Stack is empty")
+    else:
+        item = stack.pop()
+        size -= 1
+        print("Item popped:", item)
+        return size
 
+def display():
+    print("The stack is:")
+    for i in range(size-1,-1,-1):
+        print(stack[i])
 
-
+ch = "y"
+while ch == "y":
+    print("Stack operation")
+    print("1. Push")
+    print("2. Peek")
+    print("3. Is empty")
+    print("4. Pop")
+    print("5. Display")
+    print("6. Exit")
+    op = int(input("Enter option: "))
+    if op == 1:
+        size = push()
+    elif op == 2:
+        peek()
+    elif op == 3:
+        isEmpty()
+    elif op == 4:
+        size = pop()
+    elif op == 5:
+        display()
+    elif op == 6:
+        ch = "n"
+    else:
+        print("Wrong option. Try again.")
